ecd34d6e817753ae4fadb572aef0b75e0671e98a
Rlauckern <r.lauckern@gmail.com>
2022-04-12 21:30:06 +0000

    All tests passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -77,11 +77,48 @@ router.delete('/:id', async (req, res) => {
         })
     }
 })
+
 router.put('/:id', (req, res) => {
-
+    const { title, contents } = req.body;
+    if(!title || !contents) {
+        res.status(400).json({ message: "Please provide title and contents for the post" });
+    } else {
+        Post.findById(req.params.id)
+            .then(posts => {
+                if(!posts) {
+                    res.status(404).json({ message: "The post with the specified ID does not exist" });
+                } else {
+                    return Post.update(req.params.id, req.body);
+                }
+            })
+            .then(data => {
+                if(data) {
+                    return Post.findById(req.params.id);
+                }
+            })
+            .then(post => {
+                if(post) {
+                    res.status(200).json(post);
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ message: "The post information could not be modified" });
+            })
+    }
 })
-router.get('/:id', (req, res) => {
 
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if(!post) {
+            res.status(404).json({ message: "The post with the specified ID does not exist" });
+        } else {
+            const comments = await Post.findPostComments(req.params.id);
+            res.json(comments);
+        }
+    } catch (err) {
+        res.status(500).json({ message: "The comments information could not be retrieved" });
+    }
 })
 
-module.exports = router
+module.exports = router;
